a40c1b0d3fa12c54d89e49a3ca062a1d24d650b4
evanshone7 <eshone@udel.edu>
2016-04-28 13:30:59 -0400

    Updated driver

--- driver.c
+++ driver.c
@@ -6,114 +6,78 @@
 #include "heapsort.h"
 #include "quicksort.h"
 
+void printTime(char *sortType, char *arrayType, float time);
+
+int QUICK = 0;
+int MERGE = 1;
+int HEAP = 2;
+
+int (*p[3]) (int *array, int size);
+
+//main function begins...
+int main( int argc, const char* argv[] ){
+    // array of function pointers
+    p[0] = quicksort;
+    p[1] = mergesort;
+    p[2] = myheapsort;
+
+    printf("\nWe begin by measuring the time to 
+            run our sorting algorithms without optimization...\n");
   
-  
-  //main function begins...
-  int main( int argc, const char* argv[] ){
-	  
-	  
-clock_t t;
-int arraySize = 100000;
-int random[arraySize];
-int reverse[arraySize];
-int inOrder[arraySize];
-int nearlySorted[arraySize];
+    int size = 100000;
+    //Quicksort
+    printf("Quicksort is being tested with size %d...\n", size);
+    timeSort(QUICK, size);
 
+    // Mergesort 
+    printf("Mergesort is being tested with size %d...\n", size);
+    timeSort(MERGE, size);
 
-//Create arrays for testing 
-
-//inOrder array creation
-for(int i = 0; i < arraySize; i++){
-  inOrder[i] = i;
+    // Heapsort
+    printf("Heapsort is being tested with size %d...\n", size);
+    timeSort(HEAP, size);
+    return 0;
 }
-
-//reverse array creation
-for(int j = 0; j < arraySize; j++){
-  reverse[j] = arraySize - j;
-  }
-  
-//nearlySorted array creation
-for(int k = 0; k < arraySize; k++){
-
-  if(k = 99998 || k == 1){
-    nearlySorted[k] = 28;
-    }
     
-  else{
-    nearlySorted[k] = k;
+    void timeSort(int sortType, int arraySize) {
+        clock_t t;
+        int random[arraySize];
+        int reverse[arraySize];
+        int inOrder[arraySize];
+        int nearlySorted[arraySize];
+        // fill the arrays
+        createArrays(arraySize, random, reverse, inOrder, nearlySorted);
+        
+        // time the execution
+        t = clock();
+        (*p[sortType]) (random, arraySize);
+        t = clock() - t;
+        printf("sort:%d, \ttime:%f\n", sortType, t/CLOCKS_PER_SEC);
+
     }
-    
-  }
+
+    void createArrays(int arraySize, int *random, int *reverse, 
+                        int *inOrder, int *nearlySorted)
+        //inOrder array creation
+        for(int i = 0; i < arraySize; i++)
+            inOrder[i] = i;
+
+        //reverse array creation
+        for(int j = 0; j < arraySize; j++)
+            reverse[j] = arraySize - j;
   
-//random array creation
-for(int l = 0; l < arraySize; l++){
-  int r = rand() % arraySize;
-  random[l] = r;
-  }
+        //nearlySorted array creation
+        for(int k = 0; k < arraySize; k++){
+            if(k = 99998 || k == 1){
+                nearlySorted[k] = 28;
+            } else {
+                nearlySorted[k] = k;
+            }
+        }
   
-	  
-	  
-	  
-  
-  printf("\nWe begin by measuring the time to run our sorting algorithms without optimization...\n");
-  
-  //Quicksort
-  printf("Quicksort is being tested...\n");
-  
-  //Quicksort on a sorted array
-  t = clock();
-  quicksort(inOrder, arraySize);
-  t = clock() - t;
-  printf("Quicksort took  %f seconds to run on an already sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Quicksort on an inversely sorted array
-  t = clock();
-  quicksort(reverse, arraySize);
-  t = clock() - t;
-  printf("Quicksort took  %f seconds to run on a reversely sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Quicksort on a nearly sorted array
-  t = clock();
-  quicksort(nearlySorted, arraySize);
-  t = clock() - t;
-  printf("Quicksort took  %f seconds to run on a nearly sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Quicksort on a randomly sorted array
-  t = clock();
-  quicksort(random, arraySize);
-  t = clock() - t;
-  printf("Quicksort took  %f seconds to run on an randomly sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Mergesort
-  printf("Mergesort is being tested...\n");
-  
-  //Mergesort on a sorted array
-  t = clock();
-  mergesort(inOrder, arraySize);
-  t = clock() - t;
-  printf("Mergesort took  %f seconds to run on an already sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Mergesort on an inversely sorted array
-  t = clock();
-  mergesort(reverse, arraySize);
-  t = clock() - t;
-  printf("Mergesort took  %f seconds to run on a reversely sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Mergesort on a nearly sorted array
-  t = clock();
-  mergesort(nearlySorted, arraySize);
-  t = clock() - t;
-  printf("Mergesort took  %f seconds to run on a nearly sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  //Mergesort on a randomly sorted array
-  t = clock();
-  mergesort(random, arraySize);
-  t = clock() - t;
-  printf("Mergesort took  %f seconds to run on an randomly sorted array.\n",  t/CLOCKS_PER_SEC);
-  
-  
-  return 0;
-  
-  }
-  
-  
+        //random array creation
+        for(int l = 0; l < arraySize; l++){
+            int r = rand() % arraySize;
+            random[l] = r;
+        }
+    }
