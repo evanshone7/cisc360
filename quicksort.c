ce27e9e5278af27971e19a0bebae2f3442cd5fcd
evanshone7 <eshone@udel.edu>
2016-05-08 23:25:30 -0400

    Added reference

--- quicksort.c
+++ quicksort.c
@@ -3,11 +3,12 @@
 void my_quicksort(int* arr, int n) {
     quicksort_body(arr, 0, n-1);
 }
+/* taken from: http://www.comp.dit.ie/rlawlor/Alg_DS/sorting/quickSort.c */
 void quicksort_body( int a[], int l, int r) {
     int j;
 
     if( l < r ) {
-        // divide and conquer
+        /* divide and conquer */
         j = partition( a, l, r);
         quicksort_body( a, l, j-1);
         quicksort_body( a, j+1, r);
@@ -32,32 +33,3 @@ int partition( int a[], int l, int r) {
     return j;
 }
 
-
-
-/*
-void my_quicksort(int* arr, int n) {
-  quicksort_body(arr, 0, n);
-}
-
-void quicksort_body(int* arr, int left, int right) {       
-    int i = left, j = right;  
-    int tmp;
-    int pivot = arr[(left + right) / 2];
-    while (i <= j) {
-        while (arr[i] < pivot)
-            i++;  
-        while (arr[j] > pivot)    
-            j--;
-        if (i <= j) {   
-            tmp = arr[i]; 
-            arr[i] = arr[j];    
-            arr[j] = tmp; 
-            i++;    
-            j--;  
-        } 
-    }
-    if (left < j)
-        quicksort_body(arr, left, j);  
-    if (i < right)
-        quicksort_body(arr, i, right);
-}*/
